fad6b4955d28ef93d43f58e68afc2899b7329907
Ali Osman Öztürk <71381857+developeralioz@users.noreply.github.com>
2023-10-13 12:13:48 +0300

    Hafta2_1_ListBox

---
+++ Hafta2_1_ListBox/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Hafta2_1_ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 32;
+            listBox1.Items.AddRange(new object[] { "mavi", "sarı", "beyaz", "kırmızı", "yeşil" });
+            listBox1.Location = new Point(617, 369);
+            listBox1.Margin = new Padding(6, 6, 6, 6);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(219, 196);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1486, 960);
+            Controls.Add(listBox1);
+            Margin = new Padding(6, 6, 6, 6);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+    }
+}
---
+++ Hafta2_1_ListBox/Form1.cs
@@ -0,0 +1,22 @@
+﻿namespace Hafta2_1_ListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (listBox1.Text)
+            {
+                case "mavi": this.BackColor = Color.Blue; break;
+                case "sarı": this.BackColor = Color.Yellow; break;
+                case "beyaz": this.BackColor = Color.White; break;
+                case "kırmızı": this.BackColor = Color.Red; break;
+                case "yeşil": this.BackColor = Color.Green; break;
+            }
+        }
+    }
+}
