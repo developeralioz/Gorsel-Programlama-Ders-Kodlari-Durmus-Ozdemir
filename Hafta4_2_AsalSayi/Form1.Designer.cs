509309562470a679f212870d04e5a8e206438bc8
Ali Osman Öztürk <71381857+developeralioz@users.noreply.github.com>
2023-10-14 13:38:14 +0300

    Hafta4_2_AsalSayi

---
+++ Hafta4_2_AsalSayi/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Hafta4_2_AsalSayi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Sitka Display", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(186, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(426, 47);
+            label1.TabIndex = 0;
+            label1.Text = "Asal Sayı Sorgulama Programı";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(300, 143);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(145, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(300, 200);
+            button1.Name = "button1";
+            button1.Size = new Size(145, 60);
+            button1.TabIndex = 2;
+            button1.Text = "Asal mı?";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(300, 327);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 25);
+            label2.TabIndex = 3;
+            label2.Text = "label";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+    }
+}
---
+++ Hafta4_2_AsalSayi/Form1.cs
@@ -0,0 +1,47 @@
+﻿namespace Hafta4_2_AsalSayi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kontrol = 0;
+            Console.Write("Sayı Girin : ");
+            int sayi = Convert.ToInt32(textBox1.Text);
+
+            if (sayi == 1 || sayi == 0)
+            {
+                label2.Text = "Girdiğiniz Sayı Asal Değildir";
+
+            }
+            else
+            {
+                for (int i = 2; i < sayi / 2; i++)
+                {
+                    if (sayi % i == 0) //Kalansız bölünüyormu?
+                    {
+                        kontrol++;
+                        break; //Diğer sayılara bakmaya gerek duymuyoruz.
+                    }
+
+
+                }
+
+                if (kontrol != 0)
+                {
+                    label2.Text = "Girdiğiniz Sayı Asal Değildir";
+                }
+
+                else
+                {
+                    label2.Text = "Girdiğiniz Sayı Asaldır";
+                }
+
+            }
+        }
+    }
+}
